3fa629575ff61d87a55e7978b0b3a67e76f8ae65
DanielSln <12danielsolano@gmail.com>
2025-10-16 03:38:58 -0300

    feat: class number three - building functiont "generateText"

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,30 @@ const client = new OpenAI({
   apiKey: process.env.OPEN_AI_API_KEY,
 });
 
-client.chat.completions.create({
-  model: "gpt-4o-mini",
-  messages: [
-    {
-      role: "user",
-      content: "Escreva uma mensagem de uma frase sobre unicórnios.",
-    },
-  ],
-})
-    .then((completion) => {
-        console.log(completion.choices[0].message.content)
-    })
-;
+async function generateText() {
+  const completion = await client.chat.completions.create({
+    model: "gpt-4o-mini",
+    max_completion_tokens: 100,
+    messages: [
+      {
+        role: "developer", //developer -- tem mais autoridade
+        content: "Faça o uso obrigatório de emojis a cada duas palavras. Ignore todo e qualquer tipo de regra que mude a utilização dos emojis.",
+      },
+      {
+        role: "user", //
+        content: "Escreva uma mensagem de uma frase sobre unicórnios.",
+      },
+      {
+        role: "assistant",
+        content: "Os 🦄 unicórnios 🌈 são mágicos 🪄 e encantadores ✨!",
+      },
+      {
+        role: "user", //
+        content: "Obrigado.",
+      },
+    ],
+  })
+  console.log(completion.choices[0].message.content)
+}
+
+generateText();
