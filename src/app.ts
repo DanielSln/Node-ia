0360c49b9e733a3f0da5404db16154e1200d1f35
DanielSln <12danielsolano@gmail.com>
2025-10-17 00:30:50 -0300

    feat: new archive but one problem persist

--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,17 @@
-import dotenv from "dotenv";
-dotenv.config();
-import express from "express";
-import OpenAI from "openai";
-import { z } from "zod";
-import { zodResponseFormat } from "openai/helpers/zod";
+import express from 'express';
+import { generateProducts } from "./openai"
 
-const app = express();
-const client = new OpenAI({
-  apiKey: process.env.OPEN_AI_API_KEY,
-});
-app.use(express.json());
-
-const schema = z.object({
-    produtos: z.array(z.string()),
-});
+const app = express()
+app.use(express.json())
 
 app.post("/generate", async (req, res) => {
-    try {
-      const completion = await client.beta.chat.completions.parse({
-      model: "gpt-4o-mini",
-      max_completion_tokens: 100,
-      response_format: zodResponseFormat(schema, 'produtos_schema'),
-      messages: [
-        {
-        role: "developer", //developer -- tem mais autoridade
-        content: "Liste três produtos que atendam a necessidade do usuário. ",
-        },
-        {
-        role: "user", //
-        content: req.body.message,
-        },
-      ],
-    })
-
-    if(completion.choices[0].message.refusal){
-        return res.status(400).json({ error: 'Refusal'})
-    };  
-
-    res.json(completion.choices[0].message.parsed?.produtos[0])
-    } catch (e) {
-      console.log(e);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-
-})
+  try {
+    const products = await generateProducts(req.body.message)
+    res.json(products);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 export default app;
---
+++ src/openai.ts
@@ -0,0 +1,37 @@
+import dotenv from "dotenv"; 
+dotenv.config(); 
+import OpenAI from "openai"; 
+import { zodResponseFormat } from "openai/helpers/zod.mjs";
+import { z } from "zod";  
+
+const schema = z.object({
+    produtos: z.array(z.string()),
+});
+
+const client = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const generateProducts = async (message: string) => {
+    const completion = await client.chat.completions.parse({
+      model: "gpt-4o-mini",
+      max_tokens: 100,
+      response_format: zodResponseFormat(schema, "produtos_schema"),
+      messages: [
+        {
+          role: "developer",
+          content: "Liste três produtos que atendam a necessidade do usuário",
+        },
+        {
+          role: "user",
+          content: message,
+        },
+      ],
+    });
+
+    if (completion.choices[0].message.refusal) {
+        throw new Error("Refusal");
+    }
+
+    return completion.choices[0].message.parsed;
+}
