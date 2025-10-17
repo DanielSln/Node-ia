ebac0764cc3dd47732cca9f4e5e631e444567b23
DanielSln <12danielsolano@gmail.com>
2025-10-16 22:38:41 -0300

    idk

--- src/app.ts
+++ src/app.ts
@@ -2,46 +2,48 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import OpenAI from "openai";
-import {z} from "zod";
+import { z } from "zod";
 import { zodResponseFormat } from "openai/helpers/zod";
+import { Schema } from "zod/v3";
 
 const app = express();
 const client = new OpenAI({
   apiKey: process.env.OPEN_AI_API_KEY,
 });
-
 app.use(express.json());
+
+const schema = z.object({
+    produtos: z.array(z.string()),
+});
+
 app.post("/generate", async (req, res) => {
-    const completion = await client.chat.completions.create({
+    try {
+      const completion = await client.beta.chat.completions.parse({
       model: "gpt-4o-mini",
       max_completion_tokens: 100,
-      response_format: { type: "json_object" },
+      response_format: zodResponseFormat(schema, 'produtos_schema'),
       messages: [
         {
-          role: "developer", //developer -- tem mais autoridade
-          content:
-            "Liste três produtos que atendam a necessidade do usuário. Responde em JSON no formato { produtos: string[]}",
+        role: "developer", //developer -- tem mais autoridade
+        content: "Liste três produtos que atendam a necessidade do usuário. ",
         },
         {
-          role: "user", //
-          content: req.body.message,
+        role: "user", //
+        content: req.body.message,
         },
       ],
     })
 
-  const output = JSON.parse(completion.choices[0].message.content ?? "");
-  const schema = z.object({
-    produtos: z.array(z.string()),
-  })
+    if(completion.choices[0].message.refusal){
+        return res.status(400).json({ error: 'Refusal'})
+    };  
 
-  const result = schema.safeParse(output);
+    res.json(completion.choices[0].message.parsed?.produto[0])
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
 
-  if(!result.success){
-    res.status(500).end();
-    return;
-  }
-  
-  res.json({output})
 })
 
 export default app;
