96bb729f7be986f8543c966ba4f20570738b313e
DanielSln <12danielsolano@gmail.com>
2025-10-25 01:22:55 -0300

    feat: new structure code

--- src/openai.ts
+++ src/openai.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import OpenAI from "openai";
 import { zodResponseFormat } from "openai/helpers/zod";
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
 import { ChatCompletionTool } from "openai/resources/index.mjs";
 import { z } from "zod";
 
@@ -43,23 +44,25 @@ const tools: ChatCompletionTool[] = [
 ],
 
 export const generateProducts = async (message: string) => {
+  const messages: ChatCompletionMessageParam[] = [
+    {
+      role: "developer",
+      content:
+      "Liste três produtos que atendam a necessidade do usuário. Considere apenas os produtos em estoque.",
+    },
+    {
+      role: "user",
+      content: message,
+    },
+  ];
+
   const completion = await client.chat.completions.parse({
     model: "gpt-4o-mini",
     max_tokens: 100,
     response_format: zodResponseFormat(schema, "produtos"),
     tools,
-    messages: [
-      {
-        role: "developer",
-        content:
-          "Liste três produtos que atendam a necessidade do usuário. Considere apenas os produtos em estoque.",
-      },
-      {
-        role: "user",
-        content: message,
-      },
-    ],
-  });
+    messages,
+});
 
   if (completion.choices[0].message.refusal) {
     throw new Error("Refusal");
